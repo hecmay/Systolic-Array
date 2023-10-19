31f6b64adcf98fe7201588cc4c8bc25d36e6f5fa
Hecmay <sx233@cornell.edu>
2023-10-19 02:59:45 +0000

    conv

---
+++ conv/os-ro-io16-rc256-k3/src/kernel.h
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+typedef float data_t;
+#define O 16
+#define I 16
+#define R 128
+#define C 128
+#define K 3
+
+// #define O 16
+// #define I 16
+// #define R 224
+// #define C 224
+// #define K 5
